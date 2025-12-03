4a0280b8cbb3026bc92f125703706abfe43f7b4f
mohamedmamdouhhagrass-cpu <mohamedmamdouhhagrass@gmail.com>
2025-12-03 18:47:44 +0200

    Add files via upload

---
+++ mohamed.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import webbrowser
+root=Tk()
+root.geometry('500x400')
+root.title('home')
+btn1=Button(root,text='click me',width='12',height='3',bg='black',fg='white',font='25')
+btn1=Button(root,text='click me',width='12',height='3',bg='black',fg='white',font='25')
+btn1=Button(root,text='click me',width='12',height='3',bg='black',fg='white',font='25')
+btn1=Button(root,text='click me',width='12',height='3',bg='black',fg='white',font='25')
+btn1.grid(column=1 ,row=1)
+btn1.grid(column=1 ,row=2)
+btn1.grid(column=2 ,row=1)
+btn1.grid(column=2 ,row=2)
+
+root.mainloop()
